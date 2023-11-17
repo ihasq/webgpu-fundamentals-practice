f90aca551094ec131ab6b9a64ab4f99890ebbe50
ihasq <91711202+ihasq@users.noreply.github.com>
2023-11-17 15:26:25 +0900

    wip

---
+++ gputemplate/README.md
@@ -0,0 +1,17 @@
+# gputemplate
+## install
+```
+npm i gputemplate
+```
+## usage
+```javascript
+import getGPUTemplate from "gputemplate";
+
+const {
+	canvas,
+	context,
+	adapter,
+	device,
+	presentationFormat,
+} = await getGPUTemplate(document.querySelector("canvas#target"));
+```
---
+++ gputemplate/mod.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ * @param { HTMLCanvasElement | OffscreenCanvas } canvas
+ * 
+ * @returns {{
+ * 	canvas: HTMLCanvasElement | OffscreenCanvas
+ * 	context: GPUCanvasContext
+ * 	adapter: GPUAdapter,
+ * 	device: GPUDevice,
+ * 	presentationFormat: string,
+ * }}
+ * 
+ */
+
+export default async function(canvas) {
+
+	if(!("gpu" in navigator)) {
+		return undefined;
+	};
+
+	const init = Object.create(null);
+
+	init.adapter = await navigator.gpu?.requestAdapter();
+	init.device = await init.adapter?.requestDevice();
+	init.presentationFormat = navigator.gpu?.getPreferredCanvasFormat();
+
+	if(canvas && canvas instanceof (HTMLCanvasElement || OffscreenCanvas)) {
+		init.canvas = canvas;
+		init.context = init.canvas.getContext("webgpu");
+		init.context.configure({
+			device: init.device,
+			format: init.presentationFormat
+		});
+
+		if(canvas instanceof HTMLCanvasElement) {
+			Object.assign(init.canvas, {
+				width: init.canvas.clientWidth,
+				height: init.canvas.clientHeight,
+			});
+		};
+
+	};
+
+	return init;
+
+};
